63743e7f8e21a4acfff977b1d987f1c49e579c45
A-Penny <103720353+A-Penny@users.noreply.github.com>
2022-06-04 08:38:59 -0500

    add functions on db to update spent amount and create new envelopes. Also added name and id key:value functionality

--- server/db.js
+++ server/db.js
@@ -1,13 +1,23 @@
 
 class Envelope {
     constructor (budget) {
+        this.id = null;
+        this.name = ''
         this._budget = budget;
         this._balance = budget;
         this._spent = 0;
     }
 
+    get budget () {
+        return this._budget;
+    }
+
     get balance() {
         return this._balance
+    }
+
+    get spent () {
+        return this._spent;
     }
 
     set addSpend(amount) {
@@ -21,15 +31,40 @@ class Envelope {
     }
 };
 
-let housing = new Envelope (2000);
-let food = new Envelope(1500);
-let transportation = new Envelope(600);
-let entertainment = new Envelope(300);
+let idCounter = 1;
+
+// starting envelopes
+let housing = new Envelope (0);
+housing.name = 'housing';
+housing.id = idCounter++;
+let food = new Envelope(0);
+food.name = 'food';
+food.id = idCounter++;
+let transportation = new Envelope(0);
+transportation.name = 'transportation';
+transportation.id = idCounter++;
+let entertainment = new Envelope(0);
+entertainment.name = 'entertainment';
+entertainment.id = idCounter++;
 
 let envelopes = [housing, food, transportation, entertainment];
 
+// function to GET all envelopes
 const getAllEnvelopes =() => {
     return envelopes;
+}
+
+// function to update spent amount (or remove spent). Argument is the envelope to be updated
+const updateSpend = (env, amt) => {
+    env.addSpend = amt;
+    return env.balance
+}
+
+const createNewEnvelope = (name, budg) => {
+    let env = new Envelope (budg);
+    env.name = name;
+    env.id = idCounter++;
+
 }
 
 const exportedObjects = {getAllEnvelopes}
